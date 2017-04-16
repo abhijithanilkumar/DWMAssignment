0a721ab94e779a6c6401e7fe533fe9775567c006
Abhijith Anilkumar <abhijithanilkumar@live.com>
2017-04-16 23:43:00 +0530

    Changing titles

--- svm-implementation.py
+++ svm-implementation.py
@@ -7,12 +7,12 @@ X = iris.data[:, :2]
 y = iris.target
 
 c = 1.0 # SVM regularization parameter
-svc = svm.SVC(kernel='linear', C=1, gamma=0.5).fit(X,y)
+#svc = svm.SVC(kernel='linear', C=1, gamma=0.5).fit(X,y)
 #svc = svm.SVC(kernel='rbf', C=1, gamma=0.5).fit(X,y)
 #svc = svm.SVC(kernel='rbf', C=1, gamma=10).fit(X, y)
 #svc = svm.SVC(kernel='rbf', C=1, gamma=100).fit(X, y)
 #svc = svm.SVC(kernel='rbf', C=100, gamma=0.5).fit(X, y)
-#svc = svm.SVC(kernel='rbf', C=1000, gamma=0.5).fit(X, y)
+svc = svm.SVC(kernel='rbf', C=1000, gamma=0.5).fit(X, y)
 
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
@@ -29,5 +29,10 @@ plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
 plt.xlabel('Sepal length')
 plt.ylabel('Sepal width')
 plt.xlim(xx.min(), xx.max())
-plt.title('SVC with linear kernel')
+#plt.title('SVC with linear kernel : c=1 gamma=0.5')
+#plt.title('SVC with rbf kernel : c=1 gamma=0.5')
+#plt.title('SVC with rbf kernel : c=1 gamma=10')
+#plt.title('SVC with rbf kernel : c=1 gamma=100')
+#plt.title('SVC with rbf kernel : c=100 gamma=0.5')
+plt.title('SVC with rbf kernel : c=1000 gamma=0.5')
 plt.show()
